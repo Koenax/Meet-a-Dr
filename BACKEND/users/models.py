96ac997f3a3578f3951cf3596d7745761b00cdda
Keamogetsw3 <Keamogetswemaswanganyi@gmail.com>
2024-12-21 03:39:23 +0200

    added user modules

--- BACKEND/users/models.py
+++ BACKEND/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Patient modules goes here
+Class PatientProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='Patient_profile')
+    bio = models.TextField(blank=True)
+    preferences = models.JSONField(default=dict)
+
+# Doctor modules goes here
+class DoctorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='doctor_profile')
+    bio = models.TextField(blank=True)
+    address = models.TextField(null=True)
+    availability = models.JSONField(default=dict)
+    services = models.TextField()
