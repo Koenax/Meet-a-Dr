82c950a96cca83e54e0486f7ab9935a558a05f4b
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-12-21 03:58:33 +0200

    Update models.py

--- BACKEND/users/models.py
+++ BACKEND/users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Patient modules goes here
+# Patient models goes here
 class PatientProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='patient_profile')
     # Links the profile to a specific user. Each user can have only one PatientProfile.
 
-# Doctor modules goes here
+# Doctor models goes here
 class DoctorProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='doctor_profile')
     # Links the profile to a specific user.
@@ -16,7 +16,7 @@ class DoctorProfile(models.Model):
     # A services field to store a list or description of the services provided by the doctor.
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
-# Catergories modules goes here
+# Catergories models goes here
 class Category(models.Model):
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField(blank=True)
