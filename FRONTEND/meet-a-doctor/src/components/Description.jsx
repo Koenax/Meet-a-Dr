82b670f0c3eeef9b053e61cc9d776a28e344e5bf
Anuke1940 <Anukelateef@gmail.com>
2024-12-21 04:24:44 -0800

    upload viteframe work using react

---
+++ FRONTEND/meet-a-doctor/src/components/Description.jsx
@@ -0,0 +1,25 @@
+function Description() {
+  return (
+    <div className="bg-blue-500">
+        <p className="text-xs text-center uppercase pt-3">
+            Introducing 
+        </p>
+        <p className="text-3xl text-center uppercase pt-2 text-black">
+            Virtual Primary Care
+        </p>
+        <p className="text-sm pt-5 text-center pt-2">
+            We're here to help you connect every employee – to remarkable and streamlined <br />
+            text-sm care, while giving your company a better bottom line.
+        </p>
+        <div className="flex justify-center my-10">
+          <a 
+          href="#"
+          className="bg-gradient-to-r from-orange-500 to-orange-800 text-white py-2 px-3 mx-3 rounded-md mb-5">
+            Learn More
+          </a>
+        </div>
+    </div>
+  );
+}
+
+export default Description;
