312cafb16bb86c7754d0cacbb710867faeed2bdd
Keamogetsw3 <Keamogetswemaswanganyi@gmail.com>
2025-01-05 22:26:47 +0200

    Re added files to the project

--- BACKEND/meetadoctor/settings.py
+++ BACKEND/meetadoctor/settings.py
@@ -76,6 +76,8 @@ REST_AUTH = {
 INSTALLED_APPS = [
     'manageusers',
     'doctorprofile',
+    'appointments',
+    'patientfilesystem',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
