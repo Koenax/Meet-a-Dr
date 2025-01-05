f2c5174ce11cd7a9cec2964163544baf86c6185d
Keamogetsw3 <Keamogetswemaswanganyi@gmail.com>
2025-01-05 22:01:15 +0200

    Duplicate files deleted

--- new_project/doctorprofile/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import api
-
-urlpatterns = [
-    path('doctors/', api.doctor_list, name='doctor_profile_list'),
-    path('create/', api.create_doctorprofile, name='doctor_profile_create'),
-    path('<uuid:pk>/', api.doctorprofile_details, name='doctor_profile_details'),
-]
--- secondary project/listing/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import api
-
-
-urlpatterns = [
-    path('', api.doctor_list, name='doctor-list'),
-]
