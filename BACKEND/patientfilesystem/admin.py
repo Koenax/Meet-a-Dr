e9a3bc1da71240acb9334320a441616c1d22cfa4
Keamogetsw3 <Keamogetswemaswanganyi@gmail.com>
2025-01-06 19:04:59 +0200

    more files added

--- BACKEND/patientfilesystem/admin.py
+++ BACKEND/patientfilesystem/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Patient, HealthHistory, Treatment, Diagnosis, ScreeningTest
 
-# Register your models here.
+
+admin.site.register(Patient)
+admin.site.register(HealthHistory)
+admin.site.register(Treatment)
+admin.site.register(Diagnosis)
+admin.site.register(ScreeningTest)
