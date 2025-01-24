8e66d141148bd75a3adbf301b3f4c623051aad87
Mbongeni M <123000159+MbongeniChristian3@users.noreply.github.com>
2025-01-24 15:52:36 +0200

    update of app config for doctor's profile

--- BACKEND/doctorprofile/apps.py
+++ BACKEND/doctorprofile/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+# app config for doctor's profile
 class DoctorprofileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'doctorprofile'
