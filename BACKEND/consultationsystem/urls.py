9016095d1c6ac6cd1dac3d17901a13077999fff0
Koenax <138016459+Koenax@users.noreply.github.com>
2025-01-25 15:32:09 +0200

    Define URL patterns for the views created

--- BACKEND/consultationsystem/urls.py
+++ BACKEND/consultationsystem/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import api
-
+from . import views
+ 
 urlpatterns = [
     path('', api.create_booking, name='create_booking'),
     path('bookings/<uuid:pk>/confirm/', api.confirm_booking, name='confirm_booking'),
@@ -9,4 +10,8 @@ urlpatterns = [
     path('bookings/doctor/', api.get_doctor_bookings, name='get_doctor_bookings'),
 
     path('bookings/<uuid:booking_id>/initiate-call/', api.initiate_call, name='initiate_call'),
+    
+    path('start-call/<int:appointment_id>/', views.start_call, name='start_call'),
+       path('join-call/<str:meeting_id>/', views.join_call, name='join_call'),
+
 ]
