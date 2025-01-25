f7702fec91deaf32ec333bce2e95763b6ced8cb8
Koenax <138016459+Koenax@users.noreply.github.com>
2025-01-25 15:17:24 +0200

    views to handle the logic for starting, joining, and managing calls.

--- BACKEND/consultationsystem/views.py
+++ BACKEND/consultationsystem/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import CallConsultation
 
-# Create your views here.
+def start_call(request, appointment_id):
+       # Logic to start a call
+       pass
+
+def join_call(request, meeting_id):
+       call = get_object_or_404(CallConsultation, meeting_id=meeting_id)
+       # Logic to join a call
+       pass
+
+
+
